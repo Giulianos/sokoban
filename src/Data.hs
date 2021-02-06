d814846e7f3b348486f9c023ce9574a6d6314f91
Giuliano Scaglioni <giuliano.scaglioni@mercadolibre.com>
2021-02-06 06:13:44 -0300

    implement some logic with lenses

---
+++ src/Data.hs
@@ -0,0 +1,96 @@
+module Data
+    ( Object(..)
+    , Cell(..)
+    , Direction(..)
+    , Row
+    , Board
+    , objectAt
+    , emptyCell
+    , putObjectInCell
+    , canMoveObjectTo
+    , cellAt
+    , moveObject
+    , translateObject
+    , findPlayer
+    )
+where
+
+import Data.Maybe
+import Control.Lens.At
+import Control.Lens
+import Control.Lens.Indexed
+
+data Object = Player | Box deriving (Eq, Show)
+
+data Cell = Floor (Maybe Object)
+          | Storage (Maybe Object)
+          | Wall deriving (Eq, Show)
+
+type Row    = [Cell]
+type Board  = [Row]
+
+type Position = (Index Board, Index Row)
+
+data Direction = U | D | L | R
+
+objectAt :: Cell -> Maybe Object
+objectAt (Floor obj) = obj
+objectAt (Storage obj) = obj
+objectAt Wall = Nothing
+
+emptyCell :: Cell -> Cell
+emptyCell (Floor _) = Floor Nothing
+emptyCell (Storage _) = Storage Nothing
+emptyCell Wall = Wall
+
+putObjectInCell :: Cell -> Object -> Cell
+putObjectInCell (Floor Nothing) = Floor . Just
+putObjectInCell (Storage Nothing) = Storage . Just
+putObjectInCell c = const c
+
+canMoveObjectTo :: Cell -> Bool
+canMoveObjectTo (Floor Nothing) = True
+canMoveObjectTo (Storage Nothing) = True
+canMoveObjectTo _ = False
+
+moveObject :: (Cell, Cell) -> (Cell, Cell)
+moveObject (src, dst) = let srcObj = objectAt src
+                        in
+                            if canMoveObjectTo dst then
+                                (emptyCell src, maybe dst (putObjectInCell dst) srcObj)
+                            else
+                                (src, dst)
+
+pairMaybes :: Maybe a -> Maybe b -> Maybe (a, b)
+pairMaybes mx my = do x <- mx; y <- my; return (x, y)
+
+directionDelta :: Direction -> (Int, Int)
+directionDelta U = (-1, 0)
+directionDelta D = (1, 0)
+directionDelta L = (0, -1)
+directionDelta R = (0, 1)
+
+translatePos :: Position -> Direction -> Position
+translatePos (r, c) dir = let (dr, dc) = directionDelta dir in (r + dr, c + dc)
+
+cellAt :: Position -> Board -> Maybe Cell
+cellAt (row, col) = preview (ix row . ix col)
+
+setCellAt :: Position -> Cell -> Board -> Board
+setCellAt (row, col) = set (ix row . ix col)
+
+translateObject :: Position -> Direction -> Board -> Board
+translateObject pos dir board = let pos' = translatePos pos dir
+                                    cells = pairMaybes (cellAt pos board) (cellAt pos' board)
+                                in 
+                                    maybe board
+                                          (\cs -> let (src', dst') = moveObject cs in setCellAt pos src' (setCellAt pos' dst' board))
+                                          cells
+
+cellHasPlayer :: Cell -> Bool
+cellHasPlayer (Floor (Just Player)) = True
+cellHasPlayer (Storage (Just Player)) = True
+cellHasPlayer _ = False
+
+findPlayer :: Board -> Maybe Position
+findPlayer b = do (row, mcells) <- ifind (const isJust) (map (ifind (const cellHasPlayer)) b); (col, mcell) <- mcells; return (row, col)
---
+++ test/DataSpec.hs
@@ -0,0 +1,158 @@
+module DataSpec (spec) where
+
+import Test.Hspec
+import Data
+
+spec :: Spec
+spec = parallel $ do
+    describe "cellAt" $ do
+        it "returns nothing when given an empty board" $
+            cellAt (0,0) [] `shouldBe` Nothing
+    describe "cellAt (0,0)" $ do
+        it "returns top-left cell when given a non-empty board" $
+            let board = [[Floor Nothing, Wall],[Wall, Wall]] in
+                cellAt (0,0) board `shouldBe` Just (Floor Nothing)
+    describe "cellAt (0,N)" $ do
+        it "returns top-right cell when given a non-empty board" $
+            let board = [[Wall, Floor Nothing],[Wall, Wall]] in
+                cellAt (0,1) board `shouldBe` Just (Floor Nothing)
+    describe "cellAt (N,0)" $ do
+        it "returns bottom-left cell when given a non-empty board" $
+            let board = [[Wall, Wall],[Floor Nothing, Wall]] in
+                cellAt (1,0) board `shouldBe` Just (Floor Nothing)
+    describe "cellAt (N,N)" $ do
+        it "returns bottom-right cell when given a non-empty board" $
+            let board = [[Wall, Wall],[Wall, Floor Nothing]] in
+                cellAt (1,1) board `shouldBe` Just (Floor Nothing)
+    describe "objectAt" $ do
+        it "returns player when given a floor with player" $
+            let cell = Floor (Just Player) in
+                objectAt cell `shouldBe` Just Player
+        it "returns player when given a storage with player" $
+            let cell = Storage (Just Player) in
+                objectAt cell `shouldBe` Just Player
+        it "returns box when given a floor with box" $
+            let cell = Floor (Just Box) in
+                objectAt cell `shouldBe` Just Box
+        it "returns box when given a storage with box" $
+            let cell = Storage (Just Box) in
+                objectAt cell `shouldBe` Just Box
+    describe "emptyCell" $ do
+        it "returns empty floor when given a floor with player" $
+            let cell = Floor (Just Player) in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+        it "returns empty floor when given a floor with box" $
+            let cell = Floor (Just Box) in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+        it "returns empty floor when given an empty floor" $
+            let cell = Floor Nothing in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+        it "returns empty storage when given a storage with player" $
+            let cell = Storage (Just Player) in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+        it "returns empty storage when given a storage with box" $
+            let cell = Storage (Just Box) in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+        it "returns empty storage when given an empty storage" $
+            let cell = Storage Nothing in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+        it "returns wall when given a wall" $
+            let cell = Wall in
+                objectAt (emptyCell cell) `shouldBe` Nothing
+    describe "putObjectInCell" $ do
+        it "returns floor with player when given an empty floor and a player" $
+            let cell = Floor Nothing in
+                objectAt (putObjectInCell cell Player) `shouldBe` Just Player
+        it "returns floor with box when given a floor with box and a player" $
+            let cell = Floor (Just Box) in
+                objectAt (putObjectInCell cell Player) `shouldBe` Just Box
+        it "returns wall when given a wall and a player" $
+            let cell = Wall in
+                putObjectInCell cell Player `shouldBe` Wall
+    describe "canMoveObjectTo" $ do
+        it "returns True when given an empty floor" $
+            let cell = Floor Nothing in
+                canMoveObjectTo cell `shouldBe` True
+        it "returns True when given an empty storage" $
+            let cell = Storage Nothing in
+                canMoveObjectTo cell `shouldBe` True
+        it "returns False when given an storage with box" $
+            let cell = Storage (Just Box) in
+                canMoveObjectTo cell `shouldBe` False
+        it "returns False when given an floor with box" $
+            let cell = Floor (Just Box) in
+                canMoveObjectTo cell `shouldBe` False
+        it "returns False when given a wall" $
+            let cell = Wall in
+                canMoveObjectTo cell `shouldBe` False
+    describe "moveObject" $ do
+        context "when given (floor with player, empty floor)" $ do
+            it "returns (empty floor, floor with player) " $ do
+                let src = Floor (Just Player); dst = Floor Nothing in
+                    moveObject (src, dst) `shouldBe` (Floor Nothing, Floor (Just Player))
+        context "when given (floor with player, floor with box)" $ do
+            it "returns (floor with player, floor with box) " $ do
+                let src = Floor (Just Player); dst = Floor (Just Box) in
+                    moveObject (src, dst) `shouldBe` (src, dst)
+        context "when given (floor with box, empty floor)" $ do
+            it "returns (empty floor, floor with box) " $ do
+                let src = Floor (Just Box); dst = Floor Nothing in
+                    moveObject (src, dst) `shouldBe` (Floor Nothing, Floor (Just Box))
+        context "when given (empty floor, empty floor)" $ do
+            it "returns (empty floor, empty floor) " $ do
+                let src = Floor Nothing; dst = Floor Nothing in
+                    moveObject (src, dst) `shouldBe` (src, dst)
+        context "when given (floor with player, wall)" $ do
+            it "returns (floor with player, wall) " $ do
+                let src = Floor (Just Player); dst = Wall in
+                    moveObject (src, dst) `shouldBe` (src, dst)
+    describe "translateObject" $ do
+        context "when translating player up with space" $ do
+            it "returns board with player one cell up" $ do
+                translateObject (2, 1) U boardWithTopSpace `shouldBe` boardWithBottomSpace
+        context "when translating player down with space" $ do
+            it "returns board with player one cell down" $ do
+                translateObject (1, 1) D boardWithBottomSpace `shouldBe` boardWithTopSpace
+        context "when translating player left with space" $ do
+            it "returns board with player one cell left" $ do
+                translateObject (1, 2) L boardWithLeftSpace `shouldBe` boardWithRightSpace
+        context "when translating player right with space" $ do
+            it "returns board with player one cell right" $ do
+                translateObject (1, 1) R boardWithRightSpace `shouldBe` boardWithLeftSpace
+    describe "findPlayer" $ do
+        context "when given a board with a player" $ do
+            it "returns position of player in board" $ do
+                findPlayer boardWithTopSpace `shouldBe` Just (2,1)
+        context "when given a board without a player" $ do
+            it "returns nothing" $ do
+                findPlayer boardWithoutPlayer `shouldBe` Nothing
+        context "when given an empty board" $ do
+            it "returns nothing" $ do
+                findPlayer [] `shouldBe` Nothing
+
+boardWithTopSpace = [
+    [Wall,  Wall,                Wall],
+    [Wall,  Floor Nothing,       Wall],
+    [Wall,  Floor (Just Player), Wall],
+    [Wall,  Wall,                Wall]]
+
+boardWithBottomSpace = [
+    [Wall,  Wall,                Wall],
+    [Wall,  Floor (Just Player), Wall],
+    [Wall,  Floor Nothing,       Wall],
+    [Wall,  Wall,                Wall]]
+
+boardWithLeftSpace = [
+    [Wall,  Wall,                Floor Nothing, Wall],
+    [Wall,  Floor Nothing, Floor (Just Player), Wall],
+    [Wall,  Wall,                Floor Nothing, Wall]]
+
+boardWithRightSpace = [
+    [Wall,  Wall,                Floor Nothing, Wall],
+    [Wall,  Floor (Just Player), Floor Nothing, Wall],
+    [Wall,  Wall,                Floor Nothing, Wall]]
+
+boardWithoutPlayer = [
+    [Wall,  Wall,                Floor Nothing, Wall],
+    [Wall,  Floor Nothing, Floor Nothing, Wall],
+    [Wall,  Wall,                Floor Nothing, Wall]]
