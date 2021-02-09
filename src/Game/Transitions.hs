241fb783e08de23f601e61a162d23f993417ed5a
Giuliano Scaglioni <giuliano.scaglioni@mercadolibre.com>
2021-02-09 01:26:01 -0300

    add more levels and reset feature

--- src/Game/Transitions.hs
+++ src/Game/Transitions.hs
@@ -12,16 +12,24 @@ advanceLevel game = if null (remainingBoards game) then
                             }
                     else
                         let (current:remaining) = remainingBoards game
-                        in game{ playingState=Play current
+                        in game{ playingState=Play current current
                                , remainingBoards=remaining
                                }
 
+isReset :: Event -> Bool
+isReset (EventKey (Char 'r') Down _ _) = True
+isReset (EventKey (Char 'R') Down _ _) = True
+isReset  _ = False
+
 handleInput :: Event -> Game -> Game 
 handleInput event game = case playingState game
-                         of (Play  b) -> if checkFinishedBoard b then
+                         of (Play  initial b) -> if checkFinishedBoard b then
                                             game { playingState=Finish 3 }
                                          else
-                                            game{ playingState=Play (Sokoban.handleInput event b) }
+                                            if isReset event then
+                                                game{ playingState=Play initial initial }
+                                            else
+                                                game{ playingState=Play initial (Sokoban.handleInput event b) }
                             (Title timeLeft) -> if timeLeft < 0 then
                                                     advanceLevel game
                                                 else
@@ -33,6 +41,6 @@ handleInput event game = case playingState game
                                             
 timeStep :: Float -> Game -> Game
 timeStep dt game = case playingState game
-                   of (Play b) -> game
+                   of (Play initial b) -> game
                       (Finish timeLeft) -> game {playingState=Finish (timeLeft-dt)}
                       (Title timeLeft) -> game {playingState=Title (timeLeft-dt)}
--- src/Game/UI.hs
+++ src/Game/UI.hs
@@ -12,7 +12,7 @@ drawCenteredBoard b = let (rows, cols) = boardSize b
 
 drawGame :: Game -> Picture
 drawGame g = case playingState g
-             of (Play b) -> drawCenteredBoard b
+             of (Play _ b) -> drawCenteredBoard b
                 (Title _) -> color white (Translate (-75) 0 (Scale 0.2 0.2 (text "Sokoban")))
                 (Finish _) -> if finishedAllLevels g then
                                 color white (Translate (-200) 0 (Scale 0.2 0.2 (text "Todos los niveles completados!")))
